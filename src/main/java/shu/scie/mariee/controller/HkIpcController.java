19bad2b90d2bb9ac9e76d98bd962c61104fc5ff1
feidufuzhou <924262305@qq.com>
2024-06-07 16:33:34 +0800

    'add'

--- src/main/java/shu/scie/mariee/controller/HkIpcController.java
+++ src/main/java/shu/scie/mariee/controller/HkIpcController.java
@@ -291,6 +291,7 @@ public class HkIpcController {
                 Iot_url = Iotread.url;
                 value = IotService.readIOTReal(Iot_url);
                 returnList.add(String.format("%.0f ppm",value));
+                System.out.println("id:"+id);
                 returnList.add(TempPreset.need_on.get(String.valueOf(id)));//加到列表最后一个
                 return new ApiResult<>(true,returnList);
             }
